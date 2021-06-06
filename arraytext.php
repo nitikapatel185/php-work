e2bde7444fd01e2debceedd6a6741ceabe8a1fa5
Nitika Patel <68534716+nitikapatel185@users.noreply.github.com>
2021-06-06 08:12:39 +0530

    Add files via upload

---
+++ arraytext.php
@@ -0,0 +1,8 @@
+<?php
+ $personData[0]=array('Mr.','Geaoff','Hennesey','1');
+ $personData[1]=array('Mr.','Geaoff','Hennesey','2');
+ $personData[2]=array('Mr.','Geaoff','Hennesey','3');
+ $personData[3]=array('Mr.','Geaoff','Hennesey','4');
+ $personData[4]=array('Mr.','Geaoff','Hennesey','5');
+$numRows = sizeof($personData);
+ ?>
